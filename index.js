b19950d0e25d503c5728cf50037865f75c66a86a
pancake423 <58053103+pancake423@users.noreply.github.com>
2023-09-23 15:56:31 -0400

    Initial Planning
    Initial header layout, planned content to include in site body

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+Read match data from a database or static file, and load it into the website at runtime.
+Allow user to filter which matches are displayed based on a number of criteria.
+
+Required:
+Home Team
+Away Team
+Rosters
+Match scores
+Date and time
+location
+league or tournament
+Round or week information
+twitch stream or location
+
+Optional:
+Team rosters
+game scores
+VOD link (ASK ABOUT PERMANENT VODS!)
+*/
