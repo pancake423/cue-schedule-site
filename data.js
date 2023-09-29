9e362fe78a0b4f8f861b1791bb423c55b41b4a9c
pancake423 <58053103+pancake423@users.noreply.github.com>
2023-09-29 10:48:30 -0400

    added Maroon NECC schedule

--- data.js
+++ data.js
@@ -130,5 +130,101 @@ DATA = [
 	matchDate: "11/15/2023",
 	matchTime: 20, //time in 24 hour clock. converted to AM/PM
 	cardColor: "maroon"
-}
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/nighthawks.png",
+	team1Name: "Concord University Maroon",
+	team2Name: "Northern Virginia CC",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 1",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "9/29/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
+{
+	team1Logo: "logos/aloysius.jpg",
+	team2Logo: "logos/concord.png",
+	team1Name: "Mount Aloysius College",
+	team2Name: "Concord University Maroon",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 2",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "10/13/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/juniata.svg",
+	team1Name: "Concord University Maroon",
+	team2Name: "Juniata College",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 3",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "10/20/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
+{
+	team1Logo: "logos/rmu.png",
+	team2Logo: "logos/concord.png",
+	team1Name: "Robert Morris University",
+	team2Name: "Concord University",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 4",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "10/27/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/virginiatech.png",
+	team1Name: "Concord University Maroon",
+	team2Name: "Virginia Tech",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 5",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "11/3/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
+{
+	team1Logo: "logos/cumberland.svg",
+	team2Logo: "logos/concord.png",
+	team1Name: "Cumberland Univeristy Maroon",
+	team2Name: "Concord University Maroon",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 6",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "11/10/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "maroon"
+},
 ];
--- time-util.js
+++ time-util.js
@@ -5,13 +5,11 @@ function hoursToString(hrs) {
 //compares dates represented as a string of "m/d/y" aka dumb american format
 //returns -1 if a < b, 0 if a=b, 1 if a>b
 function compareDate(a, b) {
-	console.log(a, b);
 	[aM, aD, aY] = a.split("/");
 	[bM, bD, bY] = b.split("/");
 
 	const comp = Math.sign(aY - bY) * 4 + Math.sign(aM - bM) * 2 + Math.sign(aD - bD);
 
-	console.log(a, b, comp);
 	return Math.sign(comp);
 }
 
