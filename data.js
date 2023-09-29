4c5570c03b8ea06c1a2e7edc7a1c7c06e843ea51
pancake423 <58053103+pancake423@users.noreply.github.com>
2023-09-29 11:50:38 -0400

    Added (tenative) Black Schedule

--- data.js
+++ data.js
@@ -3,6 +3,8 @@ This file should either store data in a static format or contain code to extract
 */
 
 DATA = [
+
+//MAROON ECAC======================================================================================
 {
 	team1Logo: "logos/concord.png",
 	team2Logo: "logos/merrimack.png",
@@ -131,6 +133,7 @@ DATA = [
 	matchTime: 20, //time in 24 hour clock. converted to AM/PM
 	cardColor: "maroon"
 },
+//MAROON NECC======================================================================================
 {
 	team1Logo: "logos/concord.png",
 	team2Logo: "logos/nighthawks.png",
@@ -226,5 +229,199 @@ DATA = [
 	matchDate: "11/10/2023",
 	matchTime: 20, //time in 24 hour clock. converted to AM/PM
 	cardColor: "maroon"
+},
+//BLACK ECAC======================================================================================
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/siena.png",
+	team1Name: "Concord Univeristy Black",
+	team2Name: "Siena Heights University",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 1",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/10/2023",
+	matchTime: 19, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/sacred.png",
+	team1Name: "Concord Univeristy Black",
+	team2Name: "Sacred Heart University",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 3",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/11/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/csi.png",
+	team2Logo: "logos/concord.png",
+	team1Name: "College of Staten Island",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 4",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/18/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/fiu.png",
+	team1Name: "Concord Univeristy Black",
+	team2Name: "Florida International University",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 5",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/25/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/tiffin.jpg",
+	team1Name: "Concord Univeristy Black",
+	team2Name: "Tiffin University JV",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 6",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "11/1/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/ucm.svg",
+	team2Logo: "logos/concord.png",
+	team1Name: "University of Central Missouri",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 7",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "11/8/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/iona.png",
+	team2Logo: "logos/concord.png",
+	team1Name: "Iona University",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "ECAC",
+	weekOrRound: "week 8",
+	bestOf: 7,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "11/15/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+//BLACK NECC======================================================================================
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/lmc.png",
+	team1Name: "Concord University Black",
+	team2Name: "Lees-McRae College Green",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 2",
+	bestOf: 5,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/13/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/northernkentucky.png",
+	team1Name: "Northern Kentucky Univeristy",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 3",
+	bestOf: 5,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "10/20/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/concord.png",
+	team2Logo: "logos/murray.png",
+	team1Name: "Concord University Black",
+	team2Name: "Murray State",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 4",
+	bestOf: 5,
+	matchLocation: "Esports Arena",
+	videoLink: "https://www.twitch.tv/concorduesports",
+	matchDate: "10/27/2023",
+	matchTime: 21, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/thomas.png",
+	team2Logo: "logos/concord.png",
+	team1Name: "Thomas More University Blue",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 5",
+	bestOf: 5,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "11/3/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
+},
+{
+	team1Logo: "logos/indiana.svg",
+	team2Logo: "logos/concord.png",
+	team1Name: "Indiana University Crimson",
+	team2Name: "Concord University Black",
+	team1Score: "0",
+	team2Score: "0",
+	leagueName: "NECC",
+	weekOrRound: "week 6",
+	bestOf: 5,
+	matchLocation: "online",
+	videoLink: "TBD",
+	matchDate: "11/10/2023",
+	matchTime: 20, //time in 24 hour clock. converted to AM/PM
+	cardColor: "black"
 },
 ];
--- make-card.js
+++ make-card.js
@@ -1,8 +1,8 @@
 function createMatchCard(m) {
-	card = document.createElement("div");
-	mainInfo = document.createElement("div");
-	subInfo = document.createElement("div");
-	timeInfo = document.createElement("div");
+	const card = document.createElement("div");
+	const mainInfo = document.createElement("div");
+	const subInfo = document.createElement("div");
+	const timeInfo = document.createElement("div");
 
 	let dateComp = compareDate(getCurrentDate(), m.matchDate);
 	let cardShadow = "shadow-complete";
@@ -21,47 +21,53 @@ function createMatchCard(m) {
 	card.appendChild(mainInfo);
 	card.appendChild(subInfo);
 
-	team1Logo = document.createElement("img");
-	team2Logo = document.createElement("img");
+	const team1Logo = document.createElement("img");
+	const team2Logo = document.createElement("img");
 	team1Logo.className = "logo-img";
 	team2Logo.className = "logo-img";
 	team1Logo.src = m.team1Logo;
 	team2Logo.src = m.team2Logo;
 
-	team1Name = document.createElement("h2");
-	team2Name = document.createElement("h2");
+	const team1Name = document.createElement("h2");
+	const team2Name = document.createElement("h2");
 	team1Name.className = "team-name";
 	team2Name.className = "team-name";
 	team1Name.innerText = m.team1Name;
 	team2Name.innerText = m.team2Name;
 
-	team1Score = document.createElement("h1");
-	team2Score = document.createElement("h1");
+	const team1Score = document.createElement("h1");
+	const team2Score = document.createElement("h1");
 	team1Score.innerText = m.team1Score;
 	team2Score.innerText = m.team2Score;
 
-	spacer = document.createElement("div");
+	const spacer = document.createElement("div");
 	spacer.style.width = "10rem";
 
-	leagueName = document.createElement("p");
+	const leagueName = document.createElement("p");
 	leagueName.innerText = m.leagueName;
 
-	weekOrRound = document.createElement("p");
+	const weekOrRound = document.createElement("p");
 	weekOrRound.innerText = m.weekOrRound;
 
-	bestOf = document.createElement("p");
+	const bestOf = document.createElement("p");
 	bestOf.innerText = "Bo" + m.bestOf;
 
-	matchLocation = document.createElement("p");
+	const matchLocation = document.createElement("p");
 	matchLocation.innerText = m.matchLocation;
 
-	videoLink = document.createElement("a");
-	videoLink.innerText = m.videoLink;
-	videoLink.href = m.videoLink;
+	let videoLink;
+	if (isValidUrl(m.videoLink)) {
+		videoLink = document.createElement("a");
+		videoLink.innerText = m.videoLink;
+		videoLink.href = m.videoLink;
+	} else {
+		videoLink = document.createElement("p");
+		videoLink.innerText = m.videoLink;
+	}
 
-	matchDate = document.createElement("p");
+	const matchDate = document.createElement("p");
 	matchDate.innerText = m.matchDate;
-	matchTime = document.createElement("p");
+	const matchTime = document.createElement("p");
 	matchTime.innerText = hoursToString(m.matchTime);
 
 	timeInfo.appendChild(matchDate);
